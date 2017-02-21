51ab2fb769599f0a3cadd5b3a99bb6eb9af2599c
Serj T <serjtelepov@gmail.com>
2017-02-21 21:37:57 +0300

    +build

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var	plumber = require('gulp-plumber'); /*продолжает работу watch
 var postcss = require('gulp-postcss'); /*для autoprefixer*/
 var prefix = require('autoprefixer'); /*плагин для postcss*/
 var	server = require('browser-sync').create(); /*обновление localhost*/
+var rename = require("gulp-rename");
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
+var clean = require("del");
 
 gulp.task('style', function() {
 	gulp.src('less/style.less') /*берем файл для обработки*/
@@ -20,12 +25,16 @@ gulp.task('style', function() {
  				"last 2 Edge versions" ]})
 			]))
 		.pipe(gulp.dest('css')) /*складываем файл после обработки в папку*/
+		.pipe(minify())
+		.pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
 		.pipe(server.stream());
 });
 
 gulp.task('sync', ['style'], function() {
 	server.init({
-		server: ".", /*стартует из корня*/
+		server: "build", /*стартует из корня*/
 		notify: false, /*убираем оповещения*/
     open: true,
     ui: false
@@ -33,4 +42,32 @@ gulp.task('sync', ['style'], function() {
 
 	gulp.watch("less/**/*.less", ['style']);
 	gulp.watch("*.html").on("change", server.reload); /*следим за обновлениями html*/
+});
+
+gulp.task("imagemin", function() {
+  return gulp.src("img/**/*.{jpg,png}")
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**/*",
+    "js/**",
+    "*.html"
+  ], {
+  base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return clean("build");
+});
+
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", "imagemin", fn);
 });
